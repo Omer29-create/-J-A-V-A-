03c6763eed9decec63d754f99d5ed5f2addd6c98
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-12-02 17:16:30 +0100

    Add files via upload

---
+++ main/java/org/advanceJava/ch03/shape/Drawable.java
@@ -0,0 +1,7 @@
+package org.advanceJava.ch03.shape;
+
+public interface Drawable {
+	
+	void draw();
+
+}
---
+++ main/java/org/advanceJava/ch03/shape/Geometry.java
@@ -0,0 +1,12 @@
+package org.advanceJava.ch03.shape;
+
+public class Geometry {
+	
+	public void drawShape(Shape shape){
+		shape.draw();
+	}
+	
+	public void eraseShape(Shape shape){
+		shape.erase();
+	}
+}
